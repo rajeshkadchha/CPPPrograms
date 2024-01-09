ded257973d13394e33ae1563fea8a2c7d5748a83
rajeshkadchha <75838297+rajeshkadchha@users.noreply.github.com>
2024-01-09 15:12:15 +0530

    Create cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    string s;
+    char str[200];
+
+    cin>>a>>b>>c;
+    cout<<a;
+    cout<<b;
+    cout<<c;
+    return 0;
+}
